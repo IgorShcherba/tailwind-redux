74e48cfca287f3f4b985483b5a8b79caa073896c
Igor Shcherba <igorolshc@gmail.com>
2021-04-28 10:27:32 +0300

    added eslint and husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export const App = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-100 font-bold text-3xl">
-      rooqi admin panel
+      rooqi admin paneld
     </div>
   );
 };
